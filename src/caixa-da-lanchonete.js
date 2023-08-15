ccee18fb8fefafe336d5d2d03ba01e761a588e4f
Vitor Lacerda Aguirre <vitorlacerda1994@gmail.com>
2023-08-15 09:54:59 -0300

    refatoramento
    Refatoramento e correção de alguns problemas encontrados no processo

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,3 +1,4 @@
+// Definição do cardápio com os preços dos itens
 var cardapio = {
     cafe: 3,
     chantily: 1.5,
@@ -11,54 +12,68 @@ var cardapio = {
 
 class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
-        var i = 0;
-        var tamanho = itens.length;
+        var arrayItens = [];
+        // Variável para calcular o valor total da compra
         var calculo = 0;
-        var itemxquant = 0;
-        while (i < tamanho) {
-            var item = itens[i];
-            var partes = item.split(',');
-            var p1 = partes[0];
-            var p2 = parseInt(partes[1]);
-            console.log(typeof p2)
-            itemxquant = cardapio[p1] * p2;
-            calculo = calculo + itemxquant;
-            i = i + 1;
+        // Flags para verificar a presença de itens específicos
+        var temCafe = false;
+        var temSanduiche = false;
+
+        // Verifica se o carrinho está vazio
+        if (itens.length === 0) {
+            return 'Não há itens no carrinho de compra!';
         }
 
-        if (p2 === 'NaN') {
-            var result = 'Quantidade inválida!'
-        }
-        else {
+        // Loop para processar cada item no carrinho
+        for (var i = 0; i < itens.length; i++) {
+            var itemEQtd = itens[i];
+            var partes = itemEQtd.split(',');
+            var item = partes[0];
+            var quantidadeItem = partes[1];
 
-            if (metodoDePagamento == 'dinheiro') {
-                calculo = calculo + (calculo * -0.05);
-                var result = "R$ " + calculo.toFixed(2);
+            // Verifica se o item ou a quantidade são inválidos
+            if (cardapio[item] === undefined) {
+                return 'Item inválido!'
+            } else if (quantidadeItem === undefined || parseInt(quantidadeItem) === 0) {
+                return 'Quantidade inválida!'
             }
-            else if (metodoDePagamento == 'credito') {
-                calculo = calculo + (calculo * 0.03);
-                var result = "R$ " + calculo.toFixed(2);
-            }
-            else if (metodoDePagamento == 'debito') {
-                calculo = calculo;
-                var result = "R$ " + calculo.toFixed(2);
-            }
-            else {
-                var result = "Forma de pagamento inválida!";
+
+            // Adiciona o item ao carrinho e calcula o subtotal
+            arrayItens.push(item);
+            calculo += cardapio[item] * parseInt(quantidadeItem);
+
+            // Verifica se é café ou sanduíche para uso posterior
+            if (item === 'cafe') {
+                temCafe = true;
+            } else if (item === 'sanduiche') {
+                temSanduiche = true;
             }
         }
 
-        result = result.replace(/\./g, ",");
-        return result;
+        // Verifica as condições para exibir a mensagem de item extra
+        if ((arrayItens.includes('chantily') || arrayItens.includes('queijo')) && !temCafe) {
+            return 'Item extra não pode ser pedido sem o principal';
+        } else if (arrayItens.includes('queijo') && !temSanduiche) {
+            return 'Item extra não pode ser pedido sem o principal';
+        }
+
+        // Calcula o desconto ou acréscimo com base no método de pagamento
+        if (metodoDePagamento === 'dinheiro') {
+            calculo -= calculo * 0.05;
+        } else if (metodoDePagamento === 'credito') {
+            calculo += calculo * 0.03;
+        } else if (metodoDePagamento !== 'debito') {
+            return 'Forma de pagamento inválida!';
+        }
+
+        // Formata o resultado final e retorna
+        return 'R$ ' + calculo.toFixed(2).replace('.', ',');
     }
-
 }
 
-var resultado = new CaixaDaLanchonete().calcularValorDaCompra('credito', ['cafe']);
+var resultado = new CaixaDaLanchonete().calcularValorDaCompra('credito', []);
 
 
 console.log(resultado);
 
 export { CaixaDaLanchonete };
-
-
--- src/test.js
+++ src/test.js
@@ -9,17 +9,105 @@ var cardapio = {
     combo2: 7.5
 };
 
+class CaixaDaLanchonete {
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        var i = 0;
+        var tamanho = itens.length;
+        var calculo = 0;
+        var itemxquant = 0;
+        var arreyItens = [];
+        while (i < tamanho) {
+            var item = itens[i];
+            var partes = item.split(',');
+            var p1 = partes[0];
+            var p2 = partes[1];
+            itemxquant = cardapio[p1] * parseInt(p2);
+            calculo = calculo + itemxquant;
+            arreyItens[i] = p1;
+            i = i + 1;
+        }
+        console.log(arreyItens)
 
-var valorCalculado = "25";
+        if (p1 == undefined) {
+            var result = 'Não há itens no carrinho de compra!'
+        }
+        else if (!(p1 in cardapio)) {
+            var result = 'Item inválido!'
+        }
+        else if (p2 == undefined || p2 == '0') {
+            var result = 'Quantidade inválida!'
+        }
+        else if (arreyItens.includes('chantily')) {
+            if (arreyItens.includes('cafe')) {
+                if (metodoDePagamento == 'dinheiro') {
+                    calculo = calculo + (calculo * -0.05);
+                    var result = "R$ " + calculo.toFixed(2);
+                }
+                else if (metodoDePagamento == 'credito') {
+                    calculo = calculo + (calculo * 0.03);
+                    var result = "R$ " + calculo.toFixed(2);
+                }
+                else if (metodoDePagamento == 'debito') {
+                    calculo = calculo;
+                    var result = "R$ " + calculo.toFixed(2);
+                }
+                else {
+                    var result = "Forma de pagamento inválida!";
+                }
+            }
+            else {
+                var result = 'Item extra não pode ser pedido sem o principal';
+            }
+        }
+        else if (arreyItens.includes('queijo')) {
+            if (arreyItens.includes('sanduiche')) {
+                if (metodoDePagamento == 'dinheiro') {
+                    calculo = calculo + (calculo * -0.05);
+                    var result = "R$ " + calculo.toFixed(2);
+                }
+                else if (metodoDePagamento == 'credito') {
+                    calculo = calculo + (calculo * 0.03);
+                    var result = "R$ " + calculo.toFixed(2);
+                }
+                else if (metodoDePagamento == 'debito') {
+                    calculo = calculo;
+                    var result = "R$ " + calculo.toFixed(2);
+                }
+                else {
+                    var result = "Forma de pagamento inválida!";
+                }
+            }
+            else {
+                var result = 'Item extra não pode ser pedido sem o principal';
+            }
+        }
+        else {
+            if (metodoDePagamento == 'dinheiro') {
+                calculo = calculo + (calculo * -0.05);
+                var result = "R$ " + calculo.toFixed(2);
+            }
+            else if (metodoDePagamento == 'credito') {
+                calculo = calculo + (calculo * 0.03);
+                var result = "R$ " + calculo.toFixed(2);
+            }
+            else if (metodoDePagamento == 'debito') {
+                calculo = calculo;
+                var result = "R$ " + calculo.toFixed(2);
+            }
+            else {
+                var result = "Forma de pagamento inválida!";
+            }
+        }
 
-valorCalculado = parseFloat(valorCalculado);
+        result = result.replace(/\./g, ",");
+        return result;
+    }
+
+}
+
+var resultado = new CaixaDaLanchonete().calcularValorDaCompra('credito', ['chantily,2']);
 
 
+console.log(resultado);
 
-
-
-console.log(valorCalculado);
-
-
-
-
+export { CaixaDaLanchonete };
